538efc2e872539fca40246f09bca7555fe2ca3ea
offtherecord01 <liamorychlik@gmail.com>
2018-04-29 17:09:10 +0100

    First push

--- app/src/main/java/com/example/android/musicplayer/TrackAdapter.java
+++ app/src/main/java/com/example/android/musicplayer/TrackAdapter.java
@@ -1,8 +1,10 @@
 package com.example.android.musicplayer;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
